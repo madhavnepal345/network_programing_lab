53e6a5c80d3a91825251c574fef94e5091f28f4e
madhav <madhavnepal916@gmail.com>
2024-07-03 08:06:06 +0545

    Constructing Url

--- ConstructingURL.java
+++ ConstructingURL.java
@@ -3,8 +3,11 @@ import java.net.URL;
 
 public class ConstructingURL {
     public static void main(String[] args) {
+        String protocol="http";
+        String host="www.google.com";
+        String file="/bca_notes.pdf";
         try{
-            URL simpleUrl = new URL("https://www.facebook.com/");
+            URL simpleUrl = new URL(protocol,host,file);
             System.out.println("URL: " + simpleUrl.toString());
             System.out.println("Protocol: " + simpleUrl.getProtocol());
             System.out.println("Hostname: " + simpleUrl.getHost());
