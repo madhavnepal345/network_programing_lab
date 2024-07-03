39032ff9013c6494430ee6f27e9ea26975efceff
madhav <madhavnepal916@gmail.com>
2024-07-03 08:22:52 +0545

    this is url

--- ConstructingURL.java
+++ ConstructingURL.java
@@ -6,11 +6,14 @@ public class ConstructingURL {
         String protocol="http";
         String host="www.google.com";
         String file="/bca_notes.pdf";
+        int port=1045;
         try{
             URL simpleUrl = new URL(protocol,host,file);
             System.out.println("URL: " + simpleUrl.toString());
             System.out.println("Protocol: " + simpleUrl.getProtocol());
             System.out.println("Hostname: " + simpleUrl.getHost());
+            System.out.println("Port: " + simpleUrl.getPort());
+
         }catch(MalformedURLException malformedURLException){
             System.err.println(malformedURLException);
         }
