4c549bd1b0ccf85f13252981c36c49ed7e4aadaf
madhav <madhavnepal916@gmail.com>
2024-07-05 08:07:28 +0545

    Spliting Url

---
+++ SplitingURL.java
@@ -0,0 +1,20 @@
+import java.net.URL;
+import java.net.MalformedURLException;
+public class SplitingURL {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://www.bbc.com/sport/athletics/articles/cd1j3nk2183o");
+            System.out.println("Protocol: " + url.getProtocol());
+            System.out.println("Authority: " + url.getAuthority());
+            System.out.println("Host: " + url.getHost());
+            System.out.println("Port: " + url.getPort());
+            System.out.println("Path: " + url.getPath());
+            System.out.println("Query: " + url.getQuery());
+            System.out.println("File: " + url.getFile());
+            System.out.println("Ref: " + url.getRef());
+        } catch (MalformedURLException e) {
+            System.out.println("Invalid URL");
+
+}
+}
+}
